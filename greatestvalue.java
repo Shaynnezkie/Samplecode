7e8eecaa3a4b55591267fc217791fc08c56d1178
Shaineebrada <142649895+Shaynnezkie@users.noreply.github.com>
2023-10-21 11:26:40 +0800

    Add files via upload
    Signed-off-by: Shaineebrada  <142649895+Shaynnezkie@users.noreply.github.com>

---
+++ greatestvalue.java
@@ -0,0 +1,21 @@
+package ebrada;
+
+public class Greatestvalue {
+
+ public static void main (String [] arg) {
+
+int number1;
+number1 = 58;
+int number2;
+number2 = 1200;
+int number3;
+number3 = 5055;
+int highest;
+
+highest = (number1>number2)?number2:number3;
+System.out.println("number1 = "+ number1);
+System.out.println("number2 = "+ number2);
+System.out.println("number3 = "+ number3);
+System.out.println("The highest number is"+ number3);
+ }
+}
